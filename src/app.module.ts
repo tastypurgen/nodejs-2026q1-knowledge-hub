b15d96ab0ec349ebab74cfba55de042c315d9b76
tastypurgen <tastypurgen@users.noreply.github.com>
2026-05-03 11:15:55 +0200

    chore(app): wire AI and Prisma modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 
+import { AiModule } from './ai/ai.module';
 import { ArticleModule } from './article/article.module';
 import { CategoryModule } from './category/category.module';
 import { RequestLoggerMiddleware } from './common/middleware/request-logger.middleware';
 import { CommentModule } from './comment/comment.module';
+import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule, ArticleModule, CategoryModule, CommentModule],
+  imports: [PrismaModule, UserModule, ArticleModule, CategoryModule, CommentModule, AiModule],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer): void {
